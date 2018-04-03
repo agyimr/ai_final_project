73e24e05fd0b006fb0af227f7282d59d0a306ec3
Lyngsoe <s134280@student.dtu.dk>
2018-04-03 10:40:16 +0200

    Conflict class

---
+++ sampleclients/Conflicts.java
@@ -0,0 +1,16 @@
+package sampleclients;
+
+
+public class Conflicts {
+	public static char[][] MainBoard;
+	private List< Agent > agents;
+    private List< Box > boxes;
+	
+	public void detectConflict(boolean[] response, ) {
+		
+	}
+	
+	private int getConflictPartner(int id1) {
+		
+	}
+}
